b2d58052b0cd17cfde10a8487b3ef48c3a4080b7
Namanbarad <baradnaman@gmail.com>
2023-09-29 10:33:23 +0530

    c language

--- else_if1.c
+++ else_if1.c
@@ -1,39 +1,31 @@
 #include<stdio.h>
 void main()
 {
-    //    write a program to  findout small  area of two  triangle 
-   float height1,base1,area1,height2,base2,area2;
-    
-    printf("\t\ttriangle area 1\n\n");
-    printf("value of height1=  ");
-    scanf("%f",&height1);
-    printf("value of base1=  ");
-    scanf("%f",&base1);
+    int number1, number2;
+    printf("Enter number 1 ");
+    scanf("%d", &number1);
+    printf("Enter Number 2 ");
+    scanf("%d", &number2);
 
-    area1 = (height1 * base1) / 2; 
+    printf("Value of number 1 : %d \n", number1);
+    printf("Value of number 2 : %d \n", number2);
 
-    printf("\t\t triangle area 2\n\n");
-    printf("value of height2= ");
-    scanf("%f",&height2);
-    printf("value of base2= ");
-    scanf("%f",&base2);
-    area2=(height2*base2)/2;
-    printf("\n\ttriangle area 1 is %.2f\n",area1);
-    printf("\n\ttriangle area 2 is %.2f\n\n",area2);
-
-    if(area1<area2)
+    if (number1<number2)
     {
-     printf("\t\ttriangle area 1 is smaller than area 2\n");
-    
+        printf("Number 1 is smaller then number 2 \n");
+    }
+    else if(number2<number1)
+    {
+        printf("Number 2 is smaller then number 1 \n");
+
+    }
+    else if(number1==number2)
+    {
+        printf("Both are same \n");
     }
     else
     {
-     printf("\t\ttriangle area 1 is not smaller than area 2\n");
-
+        printf("Invalide input \n");
     }
-
-    printf("\t\tGOOD BY");
-
-
-
+    printf("Good bye ");
 }
--- else_if3.c
+++ else_if3.c
@@ -25,31 +25,4 @@ void main()
     }
 
     printf("\nGood bye .");
-}// write a program to check number negetive or not, if negetive to convert int to possitive
-#include<stdio.h>
-void main()
-{
-    int number;
-
-    printf("Enter Value of Number : ");
-    scanf("%d", &number);
-
-    // printf("Value of Number : %d ", number);
-
-    if(number>0)
-    {
-        printf("\nNumber is possitive and Value of number %d \n", number);
-    }
-    else if (number==0)
-    {
-        printf("\nNumber is Zero \n");
-    }
-    else
-    {
-        number = number * (-1);
-        printf("\nNumber is negetive and Value of number %d \n", number);
-
-    }
-
-    printf("\nGood bye .");
 }
