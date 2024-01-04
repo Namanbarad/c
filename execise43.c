8cb02668fe9001971e2b31c17c3b418a25259f31
Namanbarad <baradnaman@gmail.com>
2024-01-04 12:45:18 +0530

    ok

---
+++ execise43.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main() 
+
+{
+  int height,weight ,i;
+
+    printf("enter your heghit: ");
+    scanf("%d",&height);
+    printf("enter your weghit: ");
+    scanf("%d",&weight);
+   
+   for ( i=0 ;  i<5 ,         ) ;  
+   
+   
+   }
